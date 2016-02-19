3203a9042e55888af1ece8bcfbf1d1e583b37435
Maxim Stolbukhin <maxstbn@gmail.com>
2016-02-19 22:21:21 +0300

    установка заголовка страницы

--- src/components/Categories/List.js
+++ src/components/Categories/List.js
@@ -3,128 +3,63 @@
  */
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { categoryGetList, categoryCreate } from '../../redux/actions/categoryActions';
+import { categoryGetList } from '../../redux/actions/categoryActions';
 import setAppBarTitle from '../../redux/actions/configActions';
 import urls from '../../helpers/urls';
-import { routeActions } from 'react-router-redux';
-import { reduxForm } from 'redux-form';
 
 import cssClass from '../../styles/Categories.scss';
-import { List, ListItem, TextField, FlatButton, RaisedButton, Dialog } from 'material-ui';
-//import { AppBar, RaisedButton, TextField } from 'material-ui';
+import { List, ListItem, RaisedButton, FloatingActionButton, CircularProgress} from 'material-ui';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 
 export class Categories extends React.Component {
 
     constructor (props, context) {
         super(props, context);
-        this.state = {
-            open: false,
-        };
     }
 
-    handleOpen = () => {
-        this.setState({ open: true });
-    };
-
-    handleClose = () => {
-        this.setState({ open: false });
-    };
-
-    submit (values) {
-        this.props.dispatch(categoryCreate(values));
-        this.handleClose();
-    };
-
     componentWillMount () {
-        this.getUsers();
+        this.getCategoriesList();
         this.props.dispatch(setAppBarTitle(urls.categories.name))
     }
 
-    getUsers () {
+    getCategoriesList () {
         this.props.dispatch(categoryGetList())
     }
 
-    renderList () {
-        let categories = this.props.categories.list;
+    render () {
+        var categories = this.props.categories.list;
         if (categories) {
             return (
-                <List>
-                    {
-                        categories.map((item, ix) => {
-                            return (
-                                <ListItem key={ix} primaryText={item.title}>
-                                    <FlatButton
-                                        label="Отмена"
-                                        secondary={true}
-                                        onTouchTap={this.handleClose}
-                                    />
-                                </ListItem>
-                            )
-                        }, this)
-                    }
-                </List>
+                <div className={cssClass.container}>
+                    <List>
+                        {
+                            categories.map((item, ix) => {
+                                return (
+                                    <ListItem key={ix} primaryText={item.title}/>
+                                )
+                            }, this)
+                        }
+                    </List>
+                    <div className='btn-add-content'>
+                        <FloatingActionButton><ContentAdd/></FloatingActionButton>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div className="loader">
+                    <CircularProgress/>
+                </div>
             )
         }
-    }
-
-    render () {
-        const {fields: { title }, handleSubmit} = this.props;
-
-        const actions = [
-            <FlatButton
-                label="Отмена"
-                secondary={true}
-                onTouchTap={this.handleClose}
-            />,
-            <RaisedButton
-                label="Сохранить"
-                primary={true}
-                onTouchTap={handleSubmit(this.submit.bind(this))}
-            />
-        ];
-
-        return (
-            <div className={cssClass.container}>
-
-                {this.renderList()}
-
-                <RaisedButton label="Modal Dialog" onTouchTap={this.handleOpen}/>
-
-                <Dialog
-                    title='Добавление новой категории'
-                    actions={actions}
-                    modal={true}
-                    open={this.state.open}
-                >
-                    <form className={cssClass.form}>
-                        <div>
-                            <TextField
-                                fullWidth
-                                hintText="Название"
-                                {...title}
-                            />
-                        </div>
-                    </form>
-                </Dialog>
-
-            </div>
-        );
     }
 
     static propTypes = {
         dispatch: PropTypes.func.isRequired,
         categories: PropTypes.object.isRequired,
-        fields: PropTypes.object.isRequired,
-        handleSubmit: PropTypes.func.isRequired,
-        error: PropTypes.string,
-        submitting: PropTypes.bool.isRequired
+        fields: PropTypes.object.isRequired
     };
 }
-
-Categories = reduxForm({
-    form: 'category',
-    fields: ['title']
-})(Categories);
 
 function mapStateToProps (state) {
     return {
