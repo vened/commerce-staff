898922cb88723cc42bab1349888e3d227303b003
Maxim Stolbukhin <maxstbn@gmail.com>
2016-02-19 18:46:57 +0300

    категории

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,25 +4,34 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { routeActions } from 'react-router-redux';
-import {reduxForm} from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 import cssClass from '../styles/Categories.scss';
 import { categoryGetList, categoryCreate } from '../redux/actions/categoryActions';
-import { List, ListItem } from 'material-ui';
+import { List, ListItem, TextField, FlatButton, RaisedButton, Dialog } from 'material-ui';
 //import { AppBar, RaisedButton, TextField } from 'material-ui';
-
-
-const submit = (values, dispatch) => {
-    dispatch(categoryCreate(values));
-};
-
 
 export class Categories extends React.Component {
 
     constructor (props, context) {
         super(props, context);
-        this.state = {};
+        this.state = {
+            open: false,
+        };
     }
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    submit (values) {
+        this.props.dispatch(categoryCreate(values));
+        this.handleClose();
+    };
 
     componentWillMount () {
         this.getUsers();
@@ -52,19 +61,44 @@ export class Categories extends React.Component {
     render () {
         const {fields: { title }, handleSubmit} = this.props;
 
+        const actions = [
+            <FlatButton
+                label="Отмена"
+                secondary={true}
+                onTouchTap={this.handleClose}
+            />,
+            <RaisedButton
+                label="Сохранить"
+                primary={true}
+                onTouchTap={handleSubmit(this.submit.bind(this))}
+            />
+        ];
+
         return (
             <div className={cssClass.container}>
-                Categories
 
                 {this.renderList()}
 
-                <form onSubmit={handleSubmit(submit)}>
-                    <div>
-                        <label>Название</label>
-                        <input type="text" placeholder="Название" {...title}/>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
+                <RaisedButton label="Modal Dialog" onTouchTap={this.handleOpen}/>
+
+                <Dialog
+                    title='Добавление новой категории'
+                    actions={actions}
+                    modal={true}
+                    open={this.state.open}
+                >
+                    <form className={cssClass.form}>
+                        <div>
+                            <TextField
+                                fullWidth
+                                hintText="Название"
+                                {...title}
+                            />
+                        </div>
+                    </form>
+                </Dialog>
+
+
             </div>
         );
     }
